ef2f907a009b5c9c2ad0d097923594d2dfe4cbd2
amiqbal7 <iqbalmuhammad7578@gmail.com>
2024-10-20 10:38:34 +0700

    fix: handle data not available

--- resources/views/frontend/report/pdf.blade.php
+++ resources/views/frontend/report/pdf.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -37,39 +38,45 @@
         }
     </style>
 </head>
+
 <body>
     <h1>Laporan Pendapatan Asli Daerah Tahun {{ $year }}</h1>
-    <p>Periode: {{ $startDate }} s/d {{ $endDate }}</p>
+    <p>Periode: {{ \Carbon\Carbon::parse($startDate)->format('d-m-Y') }} s/d {{ \Carbon\Carbon::parse($endDate)->format('d-m-Y') }}</p>
 
-    <table border="1" cellpadding="5" cellspacing="0">
-        <thead>
-            <tr>
-                <th>NO</th>
-                <th>KODE REKENING</th>
-                <th>NAMA REKENING</th>
-                <th>TARGET (Rp.)</th>
-                <th>REALISASI (Rp.)</th>
-                <th>REALISASI %</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($realizations as $index => $realization)
+    @if ($realizations->isNotEmpty())
+        <table border="1" cellpadding="5" cellspacing="0">
+            <thead>
                 <tr>
-                    <td>{{ $index + 1 }}</td>
-                    <td>{{ $realization->rekening_code }}</td>
-                    <td>{{ $realization->rekening_name }}</td>
-                    <td>{{ number_format($realization->target, 0, ',', '.') }}</td>
-                    <td>{{ number_format($realization->total, 0, ',', '.') }}</td>
-                    <td>{{ number_format($realization->persentase_realisasi, 2) }}%</td>
+                    <th>NO</th>
+                    <th>KODE REKENING</th>
+                    <th>NAMA REKENING</th>
+                    <th>TARGET (Rp.)</th>
+                    <th>REALISASI (Rp.)</th>
+                    <th>REALISASI %</th>
                 </tr>
-            @endforeach
-            <tr>
-                <td colspan="3"><strong>TOTAL</strong></td>
-                <td><strong>{{ number_format($realizations->sum('target'), 0, ',', '.') }}</strong></td>
-                <td><strong>{{ number_format($realizations->sum('total'), 0, ',', '.') }}</strong></td>
-                <td><strong>{{ number_format(($realizations->sum('total') / $realizations->sum('target')) * 100, 2) }}%</strong></td>
-            </tr>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($realizations as $index => $realization)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $realization->rekening_code }}</td>
+                        <td>{{ $realization->rekening_name }}</td>
+                        <td>{{ number_format($realization->target, 0, ',', '.') }}</td>
+                        <td>{{ number_format($realization->total, 0, ',', '.') }}</td>
+                        <td>{{ number_format($realization->persentase_realisasi, 2) }}%</td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="3"><strong>TOTAL</strong></td>
+                    <td><strong>{{ number_format($realizations->sum('target'), 0, ',', '.') }}</strong></td>
+                    <td><strong>{{ number_format($realizations->sum('total'), 0, ',', '.') }}</strong></td>
+                    <td><strong>{{ number_format(($realizations->sum('total') / $realizations->sum('target')) * 100, 2) }}%</strong></td>
+                </tr>
+            </tbody>
+        </table>
+    @else
+        <p>Tidak ada data untuk periode {{ \Carbon\Carbon::parse($startDate)->format('d-m-Y')  }} s/d {{  \Carbon\Carbon::parse($endDate)->format('d-m-Y') }}</p>
+    @endif
 </body>
+
 </html>
